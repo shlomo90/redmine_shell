87832abfd32011f76edcd0d663e01a8ee7796b3a
임재환 <shlomolim90@gmail.com>
2022-01-10 01:44:56 +0900

    initial code

---
+++ redmine_batch/init.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * socket programming.
+ * proj_num = 1
+ *
+ * redmine init
+ *  - Init redmine workspace directory.
+ *  - Make .redmine directory.
+ *
+ * redmine init -h <url>:<port> -k <key>
+ *  - Create ".redmine" directory
+ * redmine workspace -p <project> <name>
+ *  - It should be in top directory.
+ *  - Create ".workspace" directory
+ *  - workspace path would be record at .redmine
+ *
+ * redmine journal create <issue_number>
+ * redmine journal read <issue_number> [-w file]
+ * redmine journal update <issue_number> <journal_number> [-r <file>]
+ *  - or open vi.
+ * redmine journal delete <issue_number> <journal_number>
+ *
+ * redmine issue create
+ * redmine issue read
+ * redmine issue update
+ * redmine issue delete
+ *
+ * redmine project create
+ * redmine project read
+ * redmine project update
+ * redmine project delete
+ *
+ * redmine wiki create
+ * redmine wiki read
+ * redmine wiki update
+ * redmine wiki delete
+ */
+
+extern char *optarg;
+
+struct params {
+    char    *host;
+    char    *key;
+};
+
+int parse_params(struct params *p, int argc, char* argv[])
+{
+    int      opt;
+    char    *host, *key;
+
+    /*
+     * "h:" -h argument
+     *  - if optstring is a character and followed by colon, it needs argument.
+     *
+     * "k:" -k key
+     */
+
+    if (argc == 1) {
+        fprintf(stderr, "Empty Params.\n");
+        return -1;
+    }
+
+    while ((opt = getopt(argc, argv, "h:k:")) != -1) {
+        switch (opt) {
+        case 'h':
+            // host and port.
+            p->host = optarg;
+            printf("DEBUG: -h\n");
+            break;
+        case 'k':
+            // key
+            p->key = optarg;
+            printf("DEBUG: -k\n");
+        default:
+            fprintf(stderr, "Invalid Parameter.\n");
+            return -1;
+        }
+    }
+
+    return 0;
+}
+
+int validate_params(struct params *p)
+{
+    /*
+     * Validate Host and Port number.
+     *
+     * Constraints: IP only.
+     */
+
+    //p->host
+    return 0;
+}
+
+
+int main(int argc, char* argv[])
+{
+    struct params   p;
+    int             i;
+
+    i = parse_params(&p, argc, argv);
+    return 0;
+}
