4612f018d56329ba6b8214dfd04825ed2ed106e9
임재환 <shlomolim90@gmail.com>
2022-01-13 21:46:08 +0900

    find redmine config (working...)

--- redmine_batch/red_common.h
+++ redmine_batch/red_common.h
@@ -9,7 +9,12 @@
 #include "red_string.h"
 #include "red_init.h"
 
-//
+// Max terminal length.
+// TODO: It depends on user environment.
+// Need to get conf of user terminal configuration before building.
+// (ex: getconf MAX_ARG)
+#define MAX_LINE    1048576
+
 enum red_boolean {
     RED_FALSE = 0,
     RED_TRUE
--- redmine_batch/red_init.c
+++ redmine_batch/red_init.c
@@ -80,6 +80,41 @@ static int parse_params(red_init_param_t *p, int argc, char* argv[])
     return INIT_OK;
 }
 
+/*
+ * Find redmine conf directory (".redmine") in current directory
+ * If found the redmine conf and there are valid configuration, Fill the conf
+ * and return 0. Otherwise return -1.
+ *
+ * @conf: the variable to have redmine conf
+ */
+static int find_redmine_conf(red_server_conf_t *conf)
+{
+    char        line[MAX_LINE] = {0,};
+    char       *path, *base_pos;
+    size_t      path_len;
+
+    path = getcwd(NULL, MAX_LINE);
+    path_len = strnlen(path, MAX_LINE);
+    printf("DEBUG: %s, %zu\n", path, path_len);
+
+    // target files are '.redmine/host' and '.redmine/key'.
+    // '.redmine/host' is bigger then the later. We check once.
+    if (path_len + sizeof("/.redmine/host") >= MAX_LINE) {
+        return RED_FAIL;
+    }
+
+    base_pos = &path[path_len];
+    strncat(base_pos, "/.redmine/host", 14 /*"/.redmine/host"*/);
+    printf("DEBUG: %s\n", path);
+
+    *base_pos = '\0';
+    strncat(base_pos, "/.redmine/key", 13 /*"/.redmine/key"*/);
+    printf("DEBUG: %s\n", path);
+
+    return RED_OK;
+}
+
+
 int main(int argc, char* argv[])
 {
     red_init_param_t    p;
@@ -93,9 +128,11 @@ int main(int argc, char* argv[])
         return rc;
     }
 
+    find_redmine_conf(&conf);
+
     rc = init_server_conf(&conf, &p);
     if (rc != RED_OK) {
-        fprintf(stderr, "server error.\n")
+        fprintf(stderr, "server error.\n");
     }
 
     return 0;
